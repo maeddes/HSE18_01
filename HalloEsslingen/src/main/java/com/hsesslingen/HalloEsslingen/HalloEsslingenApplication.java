d470ced1f6c683c992debee0900fbdbd22118da0
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2018-03-19 10:31:07 +0100

    first

---
+++ HalloEsslingen/src/main/java/com/hsesslingen/HalloEsslingen/HalloEsslingenApplication.java
@@ -0,0 +1,12 @@
+package com.hsesslingen.HalloEsslingen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalloEsslingenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalloEsslingenApplication.class, args);
+	}
+}
---
+++ HalloEsslingen/src/test/java/com/hsesslingen/HalloEsslingen/HalloEsslingenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hsesslingen.HalloEsslingen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HalloEsslingenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
