2a7d25b7eeace3dbc566be70a0f3e43be369ad3a
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2018-03-19 15:23:46 +0100

    first with solution

--- HalloEsslingen/src/main/java/com/hsesslingen/HalloEsslingen/HalloEsslingenApplication.java
+++ HalloEsslingen/src/main/java/com/hsesslingen/HalloEsslingen/HalloEsslingenApplication.java
@@ -1,10 +1,41 @@
 package com.hsesslingen.HalloEsslingen;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HalloEsslingenApplication {
+
+	ArrayList<String> list = new ArrayList<String>();
+
+	@GetMapping("/todos")
+	String todos(){
+
+		return list.toString();
+
+	}
+
+	@PostMapping("/add/{todo}")
+	String add(@PathVariable String todo){
+
+		list.add(todo);
+		return todo;
+	}
+
+	@DeleteMapping("/delete/{todo}")
+	String delete(@PathVariable String todo){
+
+		list.remove(todo);
+		return todo;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HalloEsslingenApplication.class, args);
